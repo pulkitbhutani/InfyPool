c4148c88fb5880fe1103afa6f429393c5c6a3c0f
Pulkit Bhutani <pulkitbhutani@outlook.com>
2020-05-29 23:02:42 +0530

    newpool update and code clean
    newpool update and code clean

--- src/app/pages/tabs/create/new-pool/new-pool.page.ts
+++ src/app/pages/tabs/create/new-pool/new-pool.page.ts
@@ -9,6 +9,7 @@ import {NavController, NavParams } from '@ionic/angular';
 import {AlertController} from '@ionic/angular';
 //import {firebase} from '@angular/fire/firebase-node';
 import * as firebase from 'firebase/app';
+import {LoadingController} from "@ionic/angular";
 
 @Component({
   selector: 'app-new-pool',
@@ -19,85 +20,72 @@ export class NewPoolPage implements OnInit {
   
   datetime = new Date().toISOString();//.getTime();
   toOffice : boolean = true;
-  destination : string;
+  destination : string = 'Infosys Jaipur';
   locations : string[];
   price : number;
   seats : number;
   createdAt = new Date();
-  mills:number;
 
   ride : Ride;
   points: Observable<any[]>;
-  backgroundColor : string;
   curDate = new Date();
-  minDate : string;
-  isValid : boolean;
-  curDateTime = new Date();
 
-  
-  //minDate = new Date().toISOString();
-  //maxDate = new Date();
-  //let firestamp = new ;
-  
-
-  constructor(db: AngularFirestore, private rideService: RideService,public alertController: AlertController, private navCtrl : NavController, private userService : UserService ) { 
-    this.points = db.collection('pickupdroppoints').valueChanges();
+  constructor(private db: AngularFirestore, private loaderCtrl : LoadingController, private rideService: RideService,public alertController: AlertController, private navCtrl : NavController, private userService : UserService ) { 
     this.ride = {};
-    
-    //this.curDate.toISOString();
-    //console.log(this.minDate);
   }
   
   ngOnInit() {
+
+    this.points = this.db.collection('pickupdroppoints').valueChanges(); 
     this.curDate.setHours(0,0,0,0);
-    this.minDate = this.curDate.toISOString();
 
-    //ride to be selected as to office by default.
-    this.toOffice = true;
-    this.destination = 'Infosys Jaipur';
-    //this.buttonColor = 'success';
-    this.backgroundColor = 'success';
-    console.log(this.minDate);
+    //this.destination = 'Infosys Jaipur';
+    
   }
 
-  todo = {}
-  logForm() {
-    console.log(this.todo)
-  }
-  
   async createRide()
-  {
+  { 
 
-    this.isValid = this.checkPoolTime(this.datetime);
-    
-    if(this.isValid)
-    {
-    //below date is formatted again to date from iso
-    this.ride.datetime = firebase.firestore.Timestamp.fromDate(new Date(this.datetime));
-    this.ride.createdAt = firebase.firestore.Timestamp.fromDate(this.createdAt);
-    this.ride.destination = this.destination;
-    this.ride.price = this.price;
-    this.ride.seats =this.seats;
-    this.ride.locations = this.locations;
-    this.ride.toOffice = this.toOffice;
-    
-    this.rideService.addRide(this.ride);
-
-    const alert = await this.alertController.create({
-      header: 'Car Pool Created',
-      //subHeader: 'Incorrect Credentials',
-      message: 'Car Pool Sucessfully Created',
-      buttons: [{
-        text :'OK',
-      handler: () => {
-        this.rideService.getRidesByUser();
-        this.navCtrl.navigateBack('/rides/tabs/create');
-      }
-      }]
+    if(this.checkPoolTime(this.datetime))
+      {
+      //below date is formatted again to date from iso
+      this.ride.datetime = firebase.firestore.Timestamp.fromDate(new Date(this.datetime));
+      this.ride.createdAt = firebase.firestore.Timestamp.fromDate(this.createdAt);
+      this.ride.destination = this.destination;
+      this.ride.price = this.price;
+      this.ride.seats =this.seats;
+      this.ride.locations = this.locations;
+      this.ride.toOffice = this.toOffice;
       
-    });
-    await alert.present();
+      this.loaderCtrl.create({
+        message:'Creating Your Ride...'
+      }).then(loadingEl=>{
+        loadingEl.present();
 
+        this.rideService.addRide(this.ride).then(res=>{
+          if(res)
+          {
+            loadingEl.dismiss();
+          }
+        }).then(async ()=>{
+
+          const alert = await this.alertController.create({
+            header: 'Car Pool Created',
+            //subHeader: 'Incorrect Credentials',
+            message: 'Car Pool Sucessfully Created',
+            buttons: [{
+              text :'OK',
+            handler: () => {
+              this.rideService.getRidesByUser();
+              this.navCtrl.navigateBack('/rides/tabs/create');
+            }
+            }]
+            
+          });
+          await alert.present();
+
+        });
+      })
     }
 
     else 
@@ -112,21 +100,30 @@ export class NewPoolPage implements OnInit {
     }
   }
 
-  clickToOffice()
+  toggleToOffice()
   {
-    this.toOffice = true;
-    this.destination = 'Infosys Jaipur';
-    //this.buttonColor = 'success';
-    this.backgroundColor = 'success';
-    console.log(this.datetime);
-    //console.log(this.datetimetest);
-    console.log(this.toOffice);
+    this.toOffice = !this.toOffice;
+
+    if(this.toOffice == true)
+    {
+      this.destination = 'Infosys Jaipur';
+      console.log('value of to office : ' + this.toOffice)
+    }
+    else
+    {
+      this.userService.getCurrentUserStartPoint().subscribe((res: UserDetail[]) => {
+        this.destination = res[0].startPoint;
+        console.log(this.destination);
+        });
+    
+        console.log(this.destination);
+    }
   }
 
   checkPoolTime(poolDateTime : string)
   {
     
-    if(this.curDateTime< new Date(poolDateTime))
+    if(new Date()< new Date(poolDateTime))
     {
       return true;
     }
@@ -136,15 +133,5 @@ export class NewPoolPage implements OnInit {
     }
   }
 
-  clickFromOffice()
-  {
-    this.toOffice = false;
-    this.userService.getCurrentUserStartPoint().subscribe((res: UserDetail[]) => {
-    this.destination = res[0].startPoint;
-    console.log(this.destination);
-    });
-
-    console.log(this.destination);
-  }
 
 }
